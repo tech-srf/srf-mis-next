e79992415700d129ca17696acc4bd149bd39944a
nashthecoder <naijeria@gmail.com>
2022-10-31 11:52:58 +0300

    broken code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
 }
 
 export default MyApp
